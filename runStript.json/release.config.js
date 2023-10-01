13872370c49dbd661955747530a01f9a29c9e23a
Sekou Dukuly <sdukuly00@gmail.com>
2023-10-01 17:35:43 -0400

    New releases

--- runStript.json/release.config.js
+++ runStript.json/release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/Life12face/gitAutomation.git", 
+    repositoryUrl: "https://github.com/Life12face/gitAutomation.git", "https://github.com/Life12face/CLD-3.git"
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
